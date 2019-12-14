c11026b67104e2fdd9e9de93d27d7aa41d0b94b8
ssizov <sergii.sizov@showmax.com>
2019-12-14 13:30:54 +0100

    use flask_restful and resources to decouple endpoints

--- mock_server/__init__.py
+++ mock_server/__init__.py
@@ -1,22 +1,13 @@
-from flask import Flask, request
-import time
+from flask import Flask
+from flask_restful import Api
+from .default_resource import DefaultResource
 
 app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(DefaultResource, '/default')
 
 
 @app.route("/")
 def root():
     return 'Welcome to Mock HTTP Server'
-
-
-@app.route("/sleep")
-def sleep():
-    sleep_time_in_seconds = request.args.get('seconds', default=0, type=float)
-    time.sleep(sleep_time_in_seconds)
-    return f'Slept for {sleep_time_in_seconds} seconds.'
-
-
-@app.route("/status")
-def http_status():
-    code = request.args.get('code', default=200, type=int)
-    return f'HTTP code: {code}.', code
